85b26f9f4eb422fb76110b2fa20cbf24d391a4ff
Zy143L <Zy143L@Dmkuai.cn>
2021-07-28 19:39:20 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import threading
 requests.packages.urllib3.disable_warnings()
 os.environ['no_proxy'] = '*'
 
-jd_ua = 'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36'
+jd_ua = 'jdapp;android;10.0.5;11;{0};network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36'
 
 
 def token_get():
     t = round(time.time())
     headers = {
-        'User-Agent': jd_ua,
+        'User-Agent': jd_ua.format(t),
         'referer': 'https://plogin.m.jd.com/cgi-bin/mm/new_login_entrance?lang=chs&appid=300&returnurl=https://wq.jd.com/passport/LoginRedirect?state={0}&returnurl=https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport'.format(t)
     }
     t = round(time.time())
@@ -29,7 +29,7 @@ def token_get():
 def token_post(s_token):
     t = round(time.time() * 1000)
     headers = {
-        'User-Agent': jd_ua,
+        'User-Agent': jd_ua.format(t),
         'referer': 'https://plogin.m.jd.com/login/login?appid=300&returnurl=https://wqlogin2.jd.com/passport/LoginRedirect?state={0}&returnurl=//home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport'.format(t),
         'Content-Type': 'application/x-www-form-urlencoded; Charset=UTF-8'
     }
@@ -56,9 +56,10 @@ def token_post(s_token):
 
 
 def check_token(token, okl_token):
+    global i
     t = round(time.time() * 1000)
     headers = {
-        'User-Agent': jd_ua,
+        'User-Agent': jd_ua.format(t),
         'referer': 'https://plogin.m.jd.com/login/login?appid=300&returnurl=https://wqlogin2.jd.com/passport/LoginRedirect?state={0}&returnurl=//home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport'.format(t),
         'Content-Type': 'application/x-www-form-urlencoded; Charset=UTF-8'
     }
@@ -84,16 +85,24 @@ def check_token(token, okl_token):
         print("已复制到剪切板")
         break
     else:
-        print(message)
-        time.sleep(1)
-        check_token(token, okl_token)
+        i = i + 1
+        # print(i)
+        if i < 30:
+            print(message)
+            time.sleep(1)
+            check_token(token, okl_token)
+        else:
+            exit(0)
 
 
 def image_print(token):
     qrurl = 'https://plogin.m.jd.com/cgi-bin/m/tmauth?client_type=m&appid=300&token={0}'.format(token)
     print("如果系统未弹出二维码图片, 请手动生成二维码")
+    print("为防止死循环 加入60次未获取到cookie后退出")
     print("URL地址已复制到剪切板, 请使用在线二维码生成")
+    print()
     print(qrurl)
+    print()
     pyperclip.copy(qrurl)
     try:
         img = qrcode.make(qrurl)
@@ -105,11 +114,12 @@ def image_print(token):
 
 if __name__ == '__main__':
     os.system('chcp 65001')
-    print("Ver: 1.0 By: limoe")
+    print("Ver: 1.2 By: limoe")
     print("https://github.com/Zy143L/jd_cookie")
     print("JD扫码获取Cookie")
     print("回车生成二维码")
     input()
+    i = 1
     s = requests.session()
     token_get()
     os.system('pause')
